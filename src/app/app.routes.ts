13069f5992be19d14490344adc3886fa36a55c07
Dieuleveut-Ngele <dieuleveutngele@gmail.com>
2024-10-07 10:22:55 +0100

    feat: added sidebar docs feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { NavBarComponent } from './pages/home/nav-bar/nav-bar.component';
+import { SidebarDocsComponent } from './pages/sidebar-docs/sidebar-docs.component';
 
 export const routes: Routes = [
     { path: '', component: NavBarComponent },
+    { path: 'docs', component: SidebarDocsComponent },
+    
 ];
---
+++ src/app/pages/sidebar-docs/sidebar-docs.component.ts
@@ -0,0 +1,46 @@
+import { CommonModule, NgClass } from '@angular/common';
+import { Component } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar-docs',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    NgClass,
+    CommonModule,
+    RouterLink
+  ],
+  templateUrl: './sidebar-docs.component.html',
+  styleUrl: './sidebar-docs.component.scss'
+})
+export class SidebarDocsComponent {
+  activeParent: string | null = null;
+  activeRoute: string = ''; 
+
+  constructor(private router: Router) {}
+
+  navigateTo(route: string) {
+    if (route === 'introduction') {
+      this.activeParent =
+        this.activeParent === 'introduction' ? null : 'introduction';
+    } else {
+      this.activeParent = null;
+    }
+
+    this.activeRoute = route; 
+    this.router.navigate(['/docs', route]);
+  }
+
+  ngOnInit() {
+    this.activeRoute = this.router.url.substring(1);
+  }
+
+  toggleParent(parent: string) {
+    this.activeParent = this.activeParent === parent ? null : parent;
+  }
+
+}
+
+
+
