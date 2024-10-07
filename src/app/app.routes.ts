81b644e42b0a14dd641d349f01f01b0042136d96
Dieuleveut-Ngele <dieuleveutngele@gmail.com>
2024-10-07 10:59:26 +0100

    feat: Added flash bank guide functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { NavBarComponent } from './pages/home/nav-bar/nav-bar.component';
 import { SidebarDocsComponent } from './pages/sidebar-docs/sidebar-docs.component';
 import { IntroductionComponent } from './components-docs/introduction/introduction.component';
+import { BulkNotificationComponent } from './components-docs/bulk-notification/bulk-notification.component';
+import { GuideFlashBankComponent } from './components-docs/guide-flash-bank/guide-flash-bank.component';
 
 export const routes: Routes = [
     { path: '', component: NavBarComponent },
@@ -10,6 +12,9 @@ export const routes: Routes = [
         component: SidebarDocsComponent,
         children: [
           { path: '', component: IntroductionComponent },
+          { path: 'introduction', component: IntroductionComponent },
+          { path: 'bulk-notification', component: BulkNotificationComponent},
+          { path: 'flash-bank', component: GuideFlashBankComponent},
         ]
     }
 ];
---
+++ src/app/components-docs/bulk-notification/bulk-notification.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bulk-notification',
+  standalone: true,
+  imports: [],
+  templateUrl: './bulk-notification.component.html',
+  styleUrl: './bulk-notification.component.scss'
+})
+export class BulkNotificationComponent {
+
+}
