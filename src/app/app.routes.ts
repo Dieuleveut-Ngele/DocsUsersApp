82269ed3d0a531dc1dc1e936927380410c54aa9e
Dieuleveut-Ngele <dieuleveutngele@gmail.com>
2024-10-07 10:39:58 +0100

    style: Added Roboto, DM Sans, Jost fonts to the project to improve typography.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { NavBarComponent } from './pages/home/nav-bar/nav-bar.component';
 import { SidebarDocsComponent } from './pages/sidebar-docs/sidebar-docs.component';
+import { IntroductionComponent } from './components-docs/introduction/introduction.component';
 
 export const routes: Routes = [
     { path: '', component: NavBarComponent },
-    { path: 'docs', component: SidebarDocsComponent },
-    
+    {
+        path: 'docs',
+        component: SidebarDocsComponent,
+        children: [
+          { path: '', component: IntroductionComponent },
+        ]
+    }
 ];
