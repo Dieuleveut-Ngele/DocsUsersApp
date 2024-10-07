10362f06ecfb93e5462aa68b132ff0219332d1c6
Dieuleveut-Ngele <dieuleveutngele@gmail.com>
2024-10-07 09:57:09 +0100

    feat: added hero section feature

--- src/app/pages/home/nav-bar/nav-bar.component.ts
+++ src/app/pages/home/nav-bar/nav-bar.component.ts
@@ -1,13 +1,15 @@
 import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, Router } from '@angular/router';
+import { HeroSectionComponent } from '../hero-section/hero-section.component';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
   imports: [
     RouterLink,
-    NgClass
+    NgClass,
+    HeroSectionComponent
   ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
