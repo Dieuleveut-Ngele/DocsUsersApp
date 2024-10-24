f4251e885c92b57c2c2dfa2645e3870ba07f8c49
Dieuleveut-Ngele <dieuleveutngele@gmail.com>
2024-10-24 11:33:18 +0100

    feat: Add background blur when popup appears

--- src/app/pages/home/nav-bar/nav-bar.component.ts
+++ src/app/pages/home/nav-bar/nav-bar.component.ts
@@ -1,96 +1,3 @@
-// import { CommonModule, NgClass } from '@angular/common';
-// import { Component } from '@angular/core';
-// import { RouterLink, RouterLinkActive, Router } from '@angular/router';
-// import { FormsModule } from '@angular/forms';
-
-
-// @Component({
-//   selector: 'app-nav-bar',
-//   standalone: true,
-//   imports: [
-//     RouterLink, 
-//     NgClass, 
-//     CommonModule, 
-//     RouterLinkActive,
-//     FormsModule,
-//  ],
-//   templateUrl: './nav-bar.component.html',
-//   styleUrl: './nav-bar.component.scss',
-// })
-// export class NavBarComponent  {
-//   activeParent: string | null = null;
-//   activeRoute: string = ''; 
-//   public showPopup = false;
-//   public searchQuery = '';
-//   searchText: string = ''; // Texte de recherche
-//   filteredDocs: { name: string, route: string }[] = []; // Stockage des résultats filtrés
-
-//   conformite = [
-//     { name: 'RakkaCash Compliance', route: 'rakka-compliance' },
-//     { name: 'Mukuru Compliance', route: 'mukuru-compliance' },
-//     { name: 'Ria Compliance', route: 'ria-compliance' },
-//     { name: 'Sababalar Compliance', route: 'saba-compliance' },
-//   ];
-
-//   marketing = [
-//     { name: 'Flash Contact', route: 'flash-contact' },
-//     { name: 'Bulk Notification', route: 'bulk-notification' }, 
-//   ];
-
-//   it = [
-//     { name: 'Reporting Flash Id', route: 'reporting-flashid' },
-//     { name: 'RakkaCash Admin', route: 'rakka-admin' },
-//     { name: 'Sababalar Admin', route: 'saba-admin' },
-//     { name: 'Ria Admin', route: 'ria-admin' }, 
-//   ];
-
-//   finance = [ 
-//     { name: 'Mukuru Finance', route: 'mukuru-finance' },
-//     { name: 'RakkaCash Finance', route: 'rakka-finance' },
-//     { name: 'Sababalar Finance', route: 'saba-finance' },
-//     { name: 'Ria Finance', route: 'ria-finance' },
-//     { name: 'Parametrage Compta', route: 'parametrage-compta' }
-//   ];
-
-//   cscOps = [
-//     { name: 'Guide Flash Cash Lite', route: 'flash-cash-lite' },
-//     { name: 'Guide Flash Marchand', route: 'flash-marchand' },
-//     { name: 'Guide FlashBank', route: 'flash-bank' },
-//     { name: 'Guide Gestion Stock', route: 'gestion-stock' },
-//     { name: 'Mukuru Operateur', route: 'mukuru-operateur' },
-//     { name: 'RakkaCash Operateur', route: 'rakka-operateur' },
-//     { name: 'Ria Operateur', route: 'ria-operateur' },
-//     { name: 'Operateur Cash In & Cash Out', route: 'operateur-cash' },
-//     { name: 'Guide Operateur Africell Data', route: 'operateur-africell-data' },
-//     { name: 'Guide Operateur Telco', route: 'operateur-telco' },
-//     { name: 'Guide Operateur VAS', route: 'operateur-vas' },
-//     { name: 'Guide Operateur Vodacom Data', route: 'operateur-vodacom-data' },
-//     { name: 'Guide Utilisateur PAYTAX', route: 'guide-paytax' },
-//     { name: 'MoneyGram', route: 'money-gram' },
-//     { name: 'Western Union', route: 'western-union' },  
-//   ];
-
-//   constructor(private router: Router) {}
-
-//   searchDocs() {
-//     const allDocs = [...this.conformite, ...this.marketing, ...this.it, ...this.finance, ...this.cscOps];
-//     this.filteredDocs = allDocs.filter(doc => doc.name.toLowerCase().includes(this.searchText.toLowerCase()));
-//   }
-
-//   navigateTo(route: string) {
-//     this.activeRoute = route;
-//     this.router.navigate(['/docs', route]);  
-//   }
-
-//   openPopup() {
-//     this.showPopup = true;
-//   }
-
-//   closePopup() {
-//     this.showPopup = false;
-//   }
-// }
-
 import { CommonModule, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, Router } from '@angular/router';
@@ -184,7 +91,6 @@ export class NavBarComponent {
   closePopup() {
     this.showPopup = false;
   }
-
 
 }
 
