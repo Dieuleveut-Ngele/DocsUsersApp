c4c9e1f5b076f54f14eb57bfcbbf5c577dd96314
Dieuleveut-Ngele <dieuleveutngele@gmail.com>
2024-10-30 12:18:37 +0100

    feat(compliance): remove top margin in generated PDF and fix line breaks

--- src/app/services/pdf-export.service.ts
+++ src/app/services/pdf-export.service.ts
@@ -8,21 +8,29 @@ export class PdfExportService {
 
   constructor() {}
 
-  exportToPDF(elementId: string, fileName: string) {
-    const element = document.getElementById(elementId);
+exportToPDF(elementId: string, fileName: string) {
+  const element = document.getElementById(elementId);
 
-    if (element) {
-      const opt = {
-        margin:       1,
-        filename:     `${fileName}.pdf`, 
-        image:        { type: 'jpeg', quality: 0.8 },
-        html2canvas:  { scale: 2, useCORS: true },
-        jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
-      };
+  if (element) {
+    // Applique la classe temporairement
+    element.classList.add('pdf-no-margin');
 
-      html2pdf().from(element).set(opt).save();
-    } else {
-      console.error(`L'élément avec l'ID "${elementId}" n'a pas été trouvé.`);
-    }
+    const opt = {
+      margin: 0.3,
+      filename: `${fileName}.pdf`,
+      image: { type: 'jpeg', quality: 0.8 },
+      html2canvas: { scale: 2, useCORS: true },
+      jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+
+    html2pdf().from(element).set(opt).save().then(() => {
+      // Retire la classe après la génération du PDF
+      element.classList.remove('pdf-no-margin');
+    });
+  } else {
+    console.error(`L'élément avec l'ID "${elementId}" n'a pas été trouvé.`);
   }
+ }
 }
+
+
